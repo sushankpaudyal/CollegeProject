728230b5e077a209412f32244d4cb53967019153
sushankpaudyal <sushankpaudyal225@gmail.com>
2025-09-07 18:40:54 +0000

    Add smooth scroll functionality and new CSS styles for the Known template
    - Updated smoothscroll.js with licensing information and author details.
    - Added a new CSS file (templatemo-style.css) for the Known template, including styles for typography, buttons, forms, sections, and responsive design.

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,8 +1,17 @@
-
+/**
+ * SmoothScroll
+ * This helper script created by DWUser.com.  Copyright 2013 DWUser.com.  
+ * Dual-licensed under the GPL and MIT licenses.  
+ * All individual scripts remain property of their copyrighters.
+ * Date: 10-Sep-2013
+ * Version: 1.0.1
+ */
 if (!window['jQuery']) alert('The jQuery library must be included before the smoothscroll.js file.  The plugin will not work propery.');
 
 /**
-
+ * jQuery.ScrollTo - Easy element scrolling using jQuery.
+ * Copyright (c) 2007-2013 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
+ * Dual licensed under MIT and GPL.
  * @author Ariel Flesler
  * @version 1.4.3.1
  */
